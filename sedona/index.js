642fe08a2fd4057e4c61554b53e2828e8c4a281d
Anuta2310 <sokolaa@gmail.com>
2023-03-16 13:42:43 +0300

    modal open / close in js

--- sedona/index.js
+++ sedona/index.js
@@ -1,3 +1,4 @@
+// Red Outline on Error
 const newsletterBtn = document.querySelector('.newsletter-btn');
 const inputEmail = document.getElementById('newsletter-email');
 
@@ -7,3 +8,17 @@ newsletterBtn.onclick = function(e){
     inputEmail.classList.add('error');
   }
 };
+
+// Modal Open / Close
+const modal = document.querySelector('.modal-container');
+const modalCloseBtn = document.querySelector('.modal-close-btn');
+const modalOpenBtns = document.querySelectorAll('.modal-open-btn');
+modalCloseBtn.onclick = function(){
+  modal.classList.add('close');
+}
+for (let modalOpenBtn of modalOpenBtns) {
+  modalOpenBtn.onclick = function(){
+    modal.classList.remove('close');
+  }
+}
+
