30ae6ca072c08616574e084260b4ad0398580eb6
Anna <sokolaa@gmail.com>
2023-04-04 15:00:54 +0300

    tabs layout and functionality

--- device/js/index.js
+++ device/js/index.js
@@ -19,3 +19,33 @@ userCart.addEventListener('click', toggleCart);
 function toggleCart(){
   popoverCart.classList.toggle('popover-open');
 }
+
+// Tabs
+
+const tabsBtn = document.querySelectorAll('.tabs__nav-btn');
+const tabsItems = document.querySelectorAll('.tabs__item');
+
+tabsBtn.forEach(onTabClick);
+
+function onTabClick(item) {
+  item.addEventListener('click', () => {
+    let currentBtn = item;
+    let tabId = currentBtn.getAttribute('data-tab');
+    let currentTab = document.querySelector(tabId);
+
+    if (!currentBtn.classList.contains('active')) {
+      tabsBtn.forEach((item) => {
+        item.classList.remove('active');
+      })
+
+      tabsItems.forEach((item) => {
+        item.classList.remove('active');
+      });
+
+      currentBtn.classList.add('active');
+      currentTab.classList.add('active');
+    }
+  });
+};
+
+document.querySelector('.tabs__nav-btn').click();
