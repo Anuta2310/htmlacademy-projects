0c562822eec93710b1893203aed263eb74140e96
Anna <sokolaa@gmail.com>
2023-05-11 11:50:19 +0300

    finished the header layout for all devices

--- barbershop2/source/js/index.js
+++ barbershop2/source/js/index.js
@@ -2,6 +2,7 @@
 const mainNav = document.querySelector('.main-nav');
 const mainNavToggle = document.querySelector('.main-nav__toggle');
 
+mainNav.classList.remove('main-nav--nojs');
 mainNavToggle.addEventListener('click', () => {
   mainNav.classList.toggle('main-nav--open');
   mainNav.classList.toggle('main-nav--closed');
